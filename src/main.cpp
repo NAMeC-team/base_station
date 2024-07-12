207041f45c30af58e75103e9078ed7a8386a7a24
Thomas Wanchai MENIER <wanchai.menier@gmail.com>
2024-07-13 01:00:51 +0200

    feat: send back feedbacks to PC via serial

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <swo.h>
 #include <rf_app.h>
 
+#define MAX_ROBOT_ID 7
 
 namespace {
 #define HALF_PERIOD 500ms
@@ -22,10 +23,50 @@ namespace {
 
 EventQueue event_queue;
 
+/**
+ * Global message response buffer
+ */
+static BaseToPC base_response = BaseToPC_init_zero;
+/**
+ * Number of robot commands to send, in a single iteration
+ * which is also the number of feedbacks to potentially receive
+ */
+static size_t num_robot_commands = 0;
+
+static bool feedbacks_sent = true;
+
+/**
+ * Timeout to trigger the function to send feedbacks to the PC
+ * if it takes too much time to receive all the feedbacks
+ */
+static Timeout feedbacks_timeout;
+
+// given by nrf24l01+ datasheet
+#define ANTENNA_SWAP_TIME 130us
+
+// timings based on packet size
+// must be changed if packets are changed
+#define BASECMD_SEND_TIME 140us
+#define ROBOT_SEND_TIME 132us
+
+/**
+ * Timeout time for a single robot
+ * Must multiply by the number of robots to get total feedback count
+ * last value is a tolerance margin
+ */
+#define FDB_TIMEOUT_TIME (BASECMD_SEND_TIME + ANTENNA_SWAP_TIME + ROBOT_SEND_TIME + 50000us)
+
 static DigitalOut led1(LED1);
 
 // SPI bus handler
 static SPI spi(SPI_MOSI_RF, SPI_MISO_RF, SPI_SCK_RF);
+
+/**
+ * Address to send commands to, and to receive feedback on
+ * Both antennas send and listen on the same address, but on
+ * different frequencies
+ */
+static uint8_t com_addr1_to_listen[5] = { 0x22, 0x87, 0xe8, 0xf9, 0x01 };
 
 /**
  * Transmission radio used to send orders to the robots
@@ -38,8 +79,18 @@ static NRF24L01 tx_radio(&spi, SPI_CS_RF1, CE_RF1, IRQ_RF1);
  * Reception radio used to listen to the robots sending
  * their feedback, on RF_FREQUENCY_2
  * Uses the antenna on COM2 port
+ * Should not be handled, use the wrapper instead
  */
 static NRF24L01 rx_radio(&spi, SPI_CS_RF2, CE_RF2, IRQ_RF2);
+
+static RF_app w_rx_radio(
+        &rx_radio,
+        RF_app::RFAppInterrupt::on_RX,
+        RF_app::RFAppMode::RX,
+        RF_FREQUENCY_2,
+        com_addr1_to_listen,
+        RadioFeedback_size
+);
 
 /**
  * Serial communication with the computer having the AI software
@@ -51,29 +102,11 @@ static UnbufferedSerial serial_port(USBTX, USBRX);
  */
 static PCToBase ai_message = PCToBase_init_zero;
 
-/**
- * Address to send commands to, and to receive feedback on
- * Both antennas send and listen on the same address, but on
- * different frequencies
- */
-static uint8_t com_addr1_to_listen[5] = { 0x22, 0x87, 0xe8, 0xf9, 0x01 };
-
 sixtron::SWO swo;
 
 FileHandle *mbed::mbed_override_console(int fd)
 {
     return &swo;
-}
-
-void save_robot_feedback() {
-    // TODO
-    led1 = !led1;
-}
-
-void send_packet(uint8_t *packet, size_t length)
-{
-    tx_radio.set_payload_size(NRF24L01::RxAddressPipe::RX_ADDR_P0, length);
-    tx_radio.send_packet(packet, length);
 }
 
 void send_protobuf_packet(BaseCommand base_cmd)
@@ -88,7 +121,7 @@ void send_protobuf_packet(BaseCommand base_cmd)
     radio_cmd.charge = base_cmd.charge;
     radio_cmd.dribbler = base_cmd.dribbler;
     radio_cmd.dev = false;
-    event_queue.call(printf, "Robot ID %d\n", radio_cmd.robot_id);
+//    event_queue.call(printf, "Robot ID %d\n", radio_cmd.robot_id);
     uint8_t tx_buffer[RadioCommand_size + 1];
 
     memset(tx_buffer, 0, sizeof(tx_buffer));
@@ -110,8 +143,57 @@ void send_protobuf_packet(BaseCommand base_cmd)
     tx_radio.send_packet(tx_buffer, RadioCommand_size + 1);
 }
 
+BaseFeedback convert_packet(RadioFeedback *to_convert) {
+    BaseFeedback fb = BaseFeedback_init_zero;
+    fb.robot_id = to_convert->robot_id;
+    fb.ir = to_convert->ir;
+    fb.motor_1_speed = to_convert->motor_1_speed;
+    fb.motor_2_speed = to_convert->motor_2_speed;
+    fb.motor_3_speed = to_convert->motor_3_speed;
+    fb.motor_4_speed = to_convert->motor_4_speed;
+    fb.voltage = to_convert->voltage;
+    return fb;
+}
+
+void send_feedbacks_to_pc()
+{
+    if (feedbacks_sent)
+        return;
+
+    serial_port.rewind();
+
+    if (base_response.feedbacks_count == 0) {
+        uint8_t buffer[1] = { 0 };
+        serial_port.write(buffer, sizeof(uint8_t));
+        serial_port.sync();
+        led1 = false;
+        return;
+    }
+
+    uint8_t tx_buffer[BaseToPC_size + 1];
+
+    pb_ostream_t tx_stream = pb_ostream_from_buffer(tx_buffer + 1, PCToBase_size);
+
+    bool status = pb_encode(&tx_stream, BaseToPC_fields, &base_response);
+
+    // `base_response` already gets filled before this function is called
+
+    if (!status)
+        return;
+
+    tx_buffer[0] = tx_stream.bytes_written;
+    serial_port.write(tx_buffer, tx_stream.bytes_written + 1);
+    serial_port.sync();
+    base_response = BaseToPC_init_zero;
+    base_response.feedbacks_count = 0;
+    num_robot_commands = 0;
+    feedbacks_sent = true;
+    led1 = false;
+}
+
 void on_rx_interrupt()
 {
+    led1 = true;
     static bool start_of_frame = false;
     static uint8_t length = 0;
     static uint8_t read_count = 0;
@@ -119,6 +201,10 @@ void on_rx_interrupt()
     uint8_t c;
 
     if (!start_of_frame) {
+        serial_port.rewind();
+        //TODO: if we exceed 6 robots, the size of the packet exceeds 8 bits,
+        // thus the length will be stored on 2 bytes, making this algorithm invalid
+        // not a problem for DivB, but it will be a problem for DivA
         serial_port.read(&c, 1);
         if (c > 0 && c <= (PCToBase_size)) { // Get packet length
             start_of_frame = true;
@@ -149,34 +235,54 @@ void on_rx_interrupt()
 
             /* Check for errors... */
             if (!status) {
-                event_queue.call(printf, "Decoding failed: %s\n", PB_GET_ERROR(&rx_stream));
+//                event_queue.call(printf, "Decoding failed: %s\n", PB_GET_ERROR(&rx_stream));
             } else {
+                if (ai_message.commands_count == 0)
+                    return;
+
+                feedbacks_sent = false;
+
+                num_robot_commands = ai_message.commands_count;
                 for (int i = 0; i < ai_message.commands_count; i++) {
                     BaseCommand command = ai_message.commands[i];
                     event_queue.call(send_protobuf_packet, command);
                 }
+                // force sending after a certain timeout if all
+                // feedbacks could not be received
+                feedbacks_timeout.attach(send_feedbacks_to_pc, num_robot_commands * FDB_TIMEOUT_TIME);
             }
         }
     }
 }
 
-void print_radio_status()
+void on_rx_rf_interrupt(uint8_t *packet, size_t data_length)
 {
-    uint8_t tx_addr_device[5] = { 0 };
+    RadioFeedback feedback = RadioFeedback_init_zero;
+    uint8_t length = packet[0];
+    if (length == 0)
+        return;
 
-    tx_radio.tx_address(tx_addr_device);
-    printf("\r\nTx Address 0x");
-    for (int i = 0; i < sizeof(tx_addr_device); i++) {
-        printf("%.2X", tx_addr_device[i]);
+    pb_istream_t rx_stream = pb_istream_from_buffer(&packet[1], length);
+    bool status = pb_decode(&rx_stream, RadioFeedback_fields, &feedback);
+
+    if (!status)
+        return;
+
+    // at this point, the new message received is valid
+    base_response.feedbacks[base_response.feedbacks_count] = convert_packet(&feedback);
+    base_response.feedbacks_count += 1;
+
+    // when all responses are received, send feedback to pc
+    if (base_response.feedbacks_count >= num_robot_commands) {
+        feedbacks_timeout.detach();
+        event_queue.call(send_feedbacks_to_pc);
     }
-    printf("\r\n");
-    printf("Radio status: 0x%x\n", tx_radio.status_register());
 }
 
 int main()
 {
     // Remote
-    serial_port.baud(115200);
+    serial_port.baud(460800);
     serial_port.attach(&on_rx_interrupt, SerialBase::RxIrq);
 
     // setup robot orders transmission radio
@@ -188,26 +294,10 @@ int main()
     tx_radio.set_interrupt(NRF24L01::InterruptMode::NONE);
 
     // setup robot feedback reception radio
-    rx_radio.initialize(
-            NRF24L01::OperationMode::RECEIVER,
-            NRF24L01::DataRate::_2MBPS,
-            RF_FREQUENCY_2);
-    rx_radio.attach_receive_payload(
-            NRF24L01::RxAddressPipe::RX_ADDR_P0,
-            com_addr1_to_listen,
-            RadioFeedback_size + 1);
-    rx_radio.set_interrupt(NRF24L01::InterruptMode::RX_ONLY);
-    rx_radio.attach(save_robot_feedback);
-    rx_radio.start_listening();
-
-    // memset(radio_packet, 0xFF, sizeof(radio_packet));
-
-    // print_radio_status();
+    w_rx_radio.attach_rx_callback(&on_rx_rf_interrupt);
+    w_rx_radio.run();
 
     event_queue.dispatch_forever();
 
-    while (true) {
-//        led1 = !led1;
-//        ThisThread::sleep_for(HALF_PERIOD);
-    }
+    while (true) {}
 }

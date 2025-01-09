6f88882aa96328f400bd22b8edcd5a0d7043da7b
Thomas Wanchai MENIER <wanchai.menier@gmail.com>
2025-01-09 15:42:55 +0100

    docs: documented code

--- src/main.cpp
+++ src/main.cpp
@@ -11,24 +11,34 @@
 
 namespace {
 #define HALF_PERIOD 500ms
-// Radio frequency
 } // namespace
 
-#define RF_FREQUENCY_1 2509
-#define RF_FREQUENCY_2 2511
+// Radio frequencies used
+#define RF_FREQUENCY_1 2509 // Base station -> Robots
+#define RF_FREQUENCY_2 2511 // Unused
 
+/**
+ * Event queue used to call execution of functions
+ * in an interrupted context
+ */
 EventQueue event_queue;
 
-static DigitalOut led1(LED1);
-static SPI spi(SPI_MOSI_RF, SPI_MISO_RF, SPI_SCK_RF);
-static NRF24L01 radio(&spi, SPI_CS_RF1, CE_RF1, IRQ_RF1);
-static UnbufferedSerial serial_port(USBTX, USBRX);
+static DigitalOut led1(LED1); // Debug led
+static SPI spi(SPI_MOSI_RF, SPI_MISO_RF, SPI_SCK_RF); // SPI handler
+static NRF24L01 radio(&spi, SPI_CS_RF1, CE_RF1, IRQ_RF1); // RF module
+static UnbufferedSerial serial_port(USBTX, USBRX); // Bidirectionnal serial comm with main computer
 
+// (global) Current AI message received,
+// updated whenever a new one is parsed
 static PCToBase ai_message = PCToBase_init_zero;
+
+// 5 bytes-long address
 static uint8_t com_addr1_to_listen[5] = { 0x22, 0x87, 0xe8, 0xf9, 0x01 };
 
+// Used to print out values via SWO wire. Data can be visualized using JLinkSWOViewer
 sixtron::SWO swo;
 
+// not sure what this does
 FileHandle *mbed::mbed_override_console(int fd)
 {
     return &swo;
@@ -42,8 +52,6 @@ void send_packet(uint8_t *packet, size_t length)
 
 void send_protobuf_packet(BaseCommand base_cmd)
 {
-    Timer t;
-    t.start();
     RadioCommand radio_cmd;
     radio_cmd.robot_id = base_cmd.robot_id;
     radio_cmd.normal_velocity = base_cmd.normal_velocity;
@@ -54,7 +62,12 @@ void send_protobuf_packet(BaseCommand base_cmd)
     radio_cmd.charge = base_cmd.charge;
     radio_cmd.dribbler = base_cmd.dribbler;
     radio_cmd.dev = false;
-    //    event_queue.call(printf, "Robot ID %d\n", radio_cmd.robot_id);
+
+    // remember that we send the packet like this
+    // 0     8       16       32 (bits)
+    // [size,         ...packet]
+    // so the robot can read first byte, to see how many bytes
+    // it has to read
     uint8_t tx_buffer[RadioCommand_size + 1];
 
     memset(tx_buffer, 0, sizeof(tx_buffer));
@@ -73,10 +86,16 @@ void send_protobuf_packet(BaseCommand base_cmd)
     }
     tx_buffer[0] = message_length;
 
-    wait_us(400);
+    wait_us(400); // required to avoid on-air packet collision
     radio.send_packet(tx_buffer, RadioCommand_size + 1);
 }
 
+/**
+ * Serial onRX interrupt function.
+ * Called whenever a new command packet from the main computer
+ * is available via USB.
+ * Once the complete packet is parsed, sends them
+ */
 void on_rx_interrupt()
 {
     static bool start_of_frame = false;
@@ -91,7 +110,6 @@ void on_rx_interrupt()
             start_of_frame = true;
             length = c;
             read_count = 0;
-            // event_queue.call(printf, "Receiving : %d\n", length);
         } else if (c == 0) { // When length is 0 it is the default protobuf packet
             start_of_frame = false;
             length = 0;
@@ -127,6 +145,9 @@ void on_rx_interrupt()
     }
 }
 
+/**
+ * Retrieve & display radio status
+ */
 void print_radio_status()
 {
     uint8_t tx_addr_device[5] = { 0 };
@@ -142,10 +163,11 @@ void print_radio_status()
 
 int main()
 {
-    // Remote
+    // Serial link with remote main software computer
     serial_port.baud(115200);
     serial_port.attach(&on_rx_interrupt, SerialBase::RxIrq);
 
+    // Initialize TX radio to transmit packets to robots
     radio.initialize(
             NRF24L01::OperationMode::TRANSCEIVER, NRF24L01::DataRate::_2MBPS, RF_FREQUENCY_1);
     radio.attach_transmitting_payload(
@@ -153,12 +175,14 @@ int main()
     radio.set_payload_size(NRF24L01::RxAddressPipe::RX_ADDR_P0, RadioCommand_size + 1);
     radio.set_interrupt(NRF24L01::InterruptMode::NONE);
 
-    // memset(radio_packet, 0xFF, sizeof(radio_packet));
-
-    // print_radio_status();
+    // Main thread execuets pending events
+    // -> Gets interrupted by Serial
+    // -> Processes incoming packet
+    // -> Queues the sending of each command for all robots
 
     event_queue.dispatch_forever();
 
+    // Fallback code if EventQueue stops
     while (true) {
         led1 = !led1;
         ThisThread::sleep_for(HALF_PERIOD);
